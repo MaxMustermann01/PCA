8f80b5f8f35a09c69ad6ad80dc91cc6cd99e0d3d
fabif <seeyoulater@herr-der-mails.de>
2014-10-28 17:48:07 +0100

    Create ApproximationOfPi.h

---
+++ exercise_1/part_2/ApproximationOfPi.h
@@ -0,0 +1,34 @@
+/*********************************************************************************
+ * FILENAME         ApproximationOfPi.h
+ * 
+ * DESCRIPTION      These functions are part of the submission to exercises of 
+ *                  the Parallel Computer Architecture (PCA) lecture of the 
+ *                  University of Heidelberg.
+ * 
+ *                  Exercise 1 - Header-File the Approximation of Pi
+ * 
+ * AUTHORS          Shamna Shyju (shamnashyju@googlemail.com)
+ *                  Fabian Finkeldey (Fabian@Finkeldey-hm.de)
+ *                  Günther Schindler (G.Schindler@stud.uni-heidelberg.de)
+ *
+ * LAST CHANGE      28. OKT 2014
+ * 
+ ********************************************************************************/
+#ifndef APPROXIMATIONOFPI_H
+#define APPROXIMATIONOFPI_H
+
+/*
+ * DESCRIPTION - will return the f(x) value to a given x
+ * PARAMETER   - X-value
+ * RETURN      - double, function-value
+ */
+double f(double);
+
+/*
+ * DESCRIPTION - will approximate pi in n steps
+ * PARAMETER   - step-count
+ * RETURN      - double approximated value for pi
+ */
+double approximatePi(int);
+
+#endif
