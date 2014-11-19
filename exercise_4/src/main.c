9d73cfaf07bdaed205aa953dc79ef7d106132eef
MaxMustermann01 <G.Schindler@stud.uni-heidelberg.de>
2014-11-19 16:50:01 +0100

    add source files for exercise 4

---
+++ exercise_4/src/main.c
@@ -0,0 +1,219 @@
+/*********************************************************************************
+ * FILENAME         main.c
+ * 
+ * DESCRIPTION      These functions are part of the submission to exercises of 
+ *                  the Parallel Computer Architecture (PCA) lecture of the 
+ *                  University of Heidelberg.
+ * 
+ *                  Exercise 4 - main function for test
+ * 
+ * AUTHORS          Shamna Shyju (shamnashyju@googlemail.com)
+ *                  Fabian Finkeldey (Fabian@Finkeldey-hm.de)
+ *                  Günther Schindler (G.Schindler@stud.uni-heidelberg.de)
+ *
+ * LAST CHANGE      19. Nov 2014
+ * 
+ ********************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "matrix_vector_mult.h"
+#include "time_measurement.h"
+
+#define SEED 4
+#define ITERATIONS 20
+
+int main(int argc, char* argv[]){
+  pthread_t * threads;    // pointer to a group of threads
+  int iSize=0, iMode=0, i, j, k;
+  double dStartTime=0.0, dElapsedTimeSer=0.0, dElapsedTimePar=0.0;
+  
+  /* Check integrity of arguments */
+  if(argc!=4)
+  {
+    printf("\nWrong number of arguments. Check usage!"
+           " \nmatrix_vector_mult <MATRIX_SIZE> <NUM_THREADS> <MODE>\n");
+    return EXIT_FAILURE;
+  }
+  /* Convert C-String to integer */
+  iSize = atoi(argv[1]);
+  iNumThreads = atoi(argv[2]);
+  iMode = atoi(argv[3]);
+  
+  if(iMode==1)
+  {
+    /* Allocate memory for matrizen */
+    vAllocMatrixInt(&Mat_Int, iSize, iSize);
+    vAllocMatrixInt(&Vec_Int, iSize, 1);
+    vAllocMatrixInt(&Res_Int, iSize, 1);
+  
+    /* Initialize matrizen with random numbers */
+    vInitMatrixInt(&Mat_Int, SEED);
+    vInitMatrixInt(&Vec_Int, SEED);
+  
+    /* Allocate memory for the thread-pointer */
+    threads = (pthread_t *) malloc(iNumThreads*sizeof(pthread_t));
+  
+    /* Start time-measurement, parallel multiply*/
+    dStartTime = dstartMesGTOD();
+    /* Start multiplication */
+    for(j = 0; j < ITERATIONS; j++)
+    {
+      for(i = 1; i < iNumThreads; i++)
+      {
+        if(pthread_create(&threads[i], NULL, (void *)vMatrixVecMulIntPar, (void *) i) != 0)
+        {
+          printf("ERROR: Couldn't create thread\n");
+          free(threads);
+          vFreeMatrixInt(&Mat_Int);
+          vFreeMatrixInt(&Vec_Int);
+          vFreeMatrixInt(&Res_Int);
+          exit(-1);
+        }
+      }
+      /* Main thread works on slice 0 */
+      vMatrixVecMulIntPar(0);
+      /* Main thread waiting for other threads to complete */
+      for(k = 1; k < iNumThreads; k++)
+        pthread_join(threads[k], NULL);
+    }
+    /* Stop time-measurement, integer */
+    dElapsedTimePar = dstopMesGTOD(dStartTime) / ITERATIONS;
+  
+    /* Write reslulting values in file, integer */
+    printf("\n Time for calculation (Intger): \n");
+    printf("\n Parallel : %lfs \n", dElapsedTimePar);
+    
+    /* Start time-measurement, parallel multiply*/
+    dStartTime = dstartMesGTOD();
+    for(j = 0; j < ITERATIONS; j++)
+      vMatrixVecMulIntSer();
+    dElapsedTimeSer = dstopMesGTOD(dStartTime) / ITERATIONS;
+    
+    printf(" Seriell  : %lfs \n", dElapsedTimeSer);
+    printf(" Speed-Up : %lf \n", dElapsedTimeSer/dElapsedTimePar);
+    /* Free Allocated Memory */
+    vFreeMatrixInt(&Mat_Int);
+    vFreeMatrixInt(&Vec_Int);
+    vFreeMatrixInt(&Res_Int);
+  }
+  else if (iMode == 2)
+  {
+    /* Allocate memory for matrizen */
+    vAllocMatrixDouble(&Mat_Double, iSize, iSize);
+    vAllocMatrixDouble(&Vec_Double, iSize, 1);
+    vAllocMatrixDouble(&Res_Double, iSize, 1);
+  
+    /* Initialize matrizen with random numbers */
+    vInitMatrixDouble(&Mat_Double, SEED);
+    vInitMatrixDouble(&Vec_Double, SEED);
+  
+    /* Allocate memory for the thread-pointer */
+    threads = (pthread_t *) malloc(iNumThreads*sizeof(pthread_t));
+  
+    /* Start time-measurement, parallel multiply*/
+    dStartTime = dstartMesGTOD();
+    /* Start multiplication */
+    for(j = 0; j < ITERATIONS; j++)
+    {
+      for(i = 1; i < iNumThreads; i++)
+      {
+        if(pthread_create(&threads[i], NULL, (void *)vMatrixVecMulDoublePar, (void *) i) != 0)
+        {
+          printf("ERROR: Couldn't create thread\n");
+          free(threads);
+          vFreeMatrixDouble(&Mat_Int);
+          vFreeMatrixDouble(&Vec_Int);
+          vFreeMatrixDouble(&Res_Int);
+          exit(-1);
+        }
+      }
+      /* Main thread works on slice 0 */
+      vMatrixVecMulDoublePar(0);
+      /* Main thread waiting for other threads to complete */
+      for(k = 1; k < iNumThreads; k++)
+        pthread_join(threads[k], NULL);
+    }
+    /* Stop time-measurement */
+    dElapsedTimePar = dstopMesGTOD(dStartTime) / ITERATIONS;
+  
+    /* Write reslulting values in file */
+    printf("\n Time for calculation (Double): \n");
+    printf("\n Parallel : %lfs \n", dElapsedTimePar);
+    
+    /* Start time-measurement, parallel multiply*/
+    dStartTime = dstartMesGTOD();
+    for(j = 0; j < ITERATIONS; j++)
+      vMatrixVecMulDoubleSer();
+    dElapsedTimeSer = dstopMesGTOD(dStartTime) / ITERATIONS;
+    
+    printf(" Seriell  : %lfs \n", dElapsedTimeSer);
+    printf(" Speed-Up : %lf \n", dElapsedTimeSer/dElapsedTimePar);
+    /* Free Allocated Memory */
+    vFreeMatrixDouble(&Mat_Double);
+    vFreeMatrixDouble(&Vec_Double);
+    vFreeMatrixDouble(&Res_Double);
+  }
+  else if (iMode == 3)
+  {
+    /* Allocate memory for matrizen */
+    vAllocMatrixFloat(&Mat_Float, iSize, iSize);
+    vAllocMatrixFloat(&Vec_Float, iSize, 1);
+    vAllocMatrixFloat(&Res_Float, iSize, 1);
+  
+    /* Initialize matrizen with random numbers */
+    vInitMatrixFloat(&Mat_Float, SEED);
+    vInitMatrixFloat(&Vec_Float, SEED);
+  
+    /* Allocate memory for the thread-pointer */
+    threads = (pthread_t *) malloc(iNumThreads*sizeof(pthread_t));
+  
+    /* Start time-measurement, parallel multiply*/
+    dStartTime = dstartMesGTOD();
+    /* Start multiplication */
+    for(j = 0; j < ITERATIONS; j++)
+    {
+      for(i = 1; i < iNumThreads; i++)
+      {
+        if(pthread_create(&threads[i], NULL, (void *)vMatrixVecMulFloatPar, (void *) i) != 0)
+        {
+          printf("ERROR: Couldn't create thread\n");
+          free(threads);
+          vFreeMatrixFloat(&Mat_Int);
+          vFreeMatrixFloat(&Vec_Int);
+          vFreeMatrixFloat(&Res_Int);
+          exit(-1);
+        }
+      }
+      /* Main thread works on slice 0 */
+      vMatrixVecMulFloatPar(0);
+      /* Main thread waiting for other threads to complete */
+      for(k = 1; k < iNumThreads; k++)
+        pthread_join(threads[k], NULL);
+    }
+    /* Stop time-measurement */
+    dElapsedTimePar = dstopMesGTOD(dStartTime) / ITERATIONS;
+  
+    /* Write reslulting values in file */
+    printf("\n Time for calculation (Float): \n");
+    printf("\n Parallel : %lfs \n", dElapsedTimePar);
+    
+    /* Start time-measurement, parallel multiply*/
+    dStartTime = dstartMesGTOD();
+    for(j = 0; j < ITERATIONS; j++)
+      vMatrixVecMulFloatSer();
+    dElapsedTimeSer = dstopMesGTOD(dStartTime) / ITERATIONS;
+    
+    printf(" Seriell  : %lfs \n", dElapsedTimeSer);
+    printf(" Speed-Up : %lf \n", dElapsedTimeSer/dElapsedTimePar);
+    /* Free Allocated Memory */
+    vFreeMatrixFloat(&Mat_Float);
+    vFreeMatrixFloat(&Vec_Float);
+    vFreeMatrixFloat(&Res_Float);
+  }
+  else
+  {
+    printf("ERROR: Wrong option (1) Integer, (2) Double, (3) Float\n");
+  }
+  return EXIT_SUCCESS;
+}
---
+++ exercise_4/src/matrix_vector_mult.h
@@ -0,0 +1,100 @@
+/*********************************************************************************
+ * FILENAME         matrix_vector_mult.h
+ * 
+ * DESCRIPTION      These functions are part of the submission to exercises of 
+ *                  the Parallel Computer Architecture (PCA) lecture of the 
+ *                  University of Heidelberg.
+ * 
+ *                  Exercise 4 - Header-File the Matrix-Vector-Multiplication
+ * 
+ * AUTHORS          Shamna Shyju (shamnashyju@googlemail.com)
+ *                  Fabian Finkeldey (Fabian@Finkeldey-hm.de)
+ *                  Günther Schindler (G.Schindler@stud.uni-heidelberg.de)
+ *
+ * LAST CHANGE      19. Nov 2014
+ * 
+ ********************************************************************************/
+#ifndef MATRIX_VECTOR_MULT_H
+#define MATRIX_VECTOR_MULT_H
+
+typedef struct sMatrixInt
+{
+  int iRow;       // for rows
+  int iCol;       // for columns
+  int** ppaMat;   // int[][]
+} sMatrixInt;
+
+typedef struct sMatrixDouble
+{
+  int iRow;       // for rows
+  int iCol;       // for columns
+  double** ppaMat;   // int[][]
+} sMatrixDouble;
+
+typedef struct sMatrixFloat
+{
+  int iRow;       // for rows
+  int iCol;       // for columns
+  float** ppaMat;   // int[][]
+} sMatrixFloat;
+
+sMatrixInt Mat_Int, Vec_Int, Res_Int;
+sMatrixDouble Mat_Double, Vec_Double, Res_Double;
+sMatrixInt Mat_Float, Vec_Float, Res_Float;
+int iNumThreads;
+
+/* 
+ * DESCRIPTION - Multiply a matrix by a vector, seriel; either int, double,float
+ * PARAMETER   - void
+ * RETURN      - void
+ */
+void vMatrixVecMulIntSer(void);
+void vMatrixVecMulDoubleSer(void);
+void vMatrixVecMulFloatSer(void);
+
+/* 
+ * DESCRIPTION - Multiply a matrix by a vector, parallel; either int, double,float
+ * PARAMETER   - type * : number of slices
+ * RETURN      - void
+ */
+void vMatrixVecMulIntPar(int *);
+void vMatrixVecMulDoublePar(int *);
+void vMatrixVecMulFloatPar(int *);
+
+/*
+ * DESCRIPTION - Allocate memory for the dynamic matrix; either int, double, float
+ * PARAMETER   - sMatrixInt: pointer to a sMatrixInt type for which the memory will be allocated
+ *             - Integer: matrix rows
+ *             - Integer: matrix columns
+ *RETURN       - Integer: 0 if alloc was ok, 1 if alloc failed 
+ */
+int vAllocMatrixInt(sMatrixInt *,int, int);
+int vAllocMatrixDouble(sMatrixDouble *,int, int);
+int vAllocMatrixFloat(sMatrixFloat *,int, int);
+/*
+ * DESCRIPTION - will free the allocated memory; either int, double, float
+ * PARAMETER   - pointer to type sMatrixInt, which should be destroid
+ * RETURN      - void
+ */
+void vFreeMatrixInt(sMatrixInt *);
+void vFreeMatrixDouble(sMatrixDouble *);
+void vFreeMatrixFloat(sMatrixFloat *);
+/*
+ * DESCRIPTION - prints the matrix-elements; either int, double, float; only for test
+ * PARAMETER   - pointer to matrix, which should be printed
+ * RETURN      - void
+ */
+void vPrintMatrixInt(sMatrixInt *);
+void vPrintMatrixDouble(sMatrixDouble *);
+void vPrintMatrixFloat(sMatrixFloat *);
+/*
+ * DESCRIPTION - initialize a matrix with random numbers, either int, double, float
+ * PARAMETER   - pointer to matrix...
+ *               integer for seed
+ * RETRUN      - void
+ */
+void vInitMatrixInt(sMatrixInt *, int);
+void vInitMatrixDouble(sMatrixDouble *, int);
+void vInitMatrixFloat(sMatrixFloat *, int);
+
+#endif
